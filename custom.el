7f7f142ac9b7ade66e098c23dc49124d19ca4c0f
evie anderson <git@evie-anderson.com>
2026-08-31 09:49:53 -0400

    list

--- custom.el
+++ custom.el
@@ -5,21 +5,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/org/school/cis-3217.org" "/home/evie/org/school/cis-3296.org"
-     "/home/evie/org/school/cis-3441.org" "/home/evie/org/school/cis-4345.org"
-     "/home/evie/org/personal.org" "/home/evie/org/todo.org"
-     "/home/evie/org/school/fall26.org" "/home/evie/org/car.org"
-     "/home/evie/org/declutter.org" "/home/evie/org/editing.org"
-     "/home/evie/org/finance.org" "/home/evie/org/fitness.org"
-     "/home/evie/org/four_month_plan.org" "/home/evie/org/france-guide.org"
-     "/home/evie/org/france26.org" "/home/evie/org/ghc.org"
-     "/home/evie/org/homeserver.org" "/home/evie/org/jobs.org"
-     "/home/evie/org/kitchen.org" "/home/evie/org/life.org"
-     "/home/evie/org/medical.org" "/home/evie/org/nix-migration.org"
-     "/home/evie/org/projects.org" "/home/evie/org/reading.org"
-     "/home/evie/org/staffportal.org" "/home/evie/org/summer.org"
-     "/home/evie/org/unisys.org" "/home/evie/org/weeks.org"
-     "/home/evie/org/winston.org"))
+   '("~/org/school/cis-1001.org" "/home/evie/org/school/cis-3217.org"
+     "/home/evie/org/school/cis-3296.org" "/home/evie/org/school/cis-3441.org"
+     "/home/evie/org/school/cis-4345.org" "/home/evie/org/personal.org"
+     "/home/evie/org/todo.org" "/home/evie/org/school/fall26.org"
+     "/home/evie/org/car.org" "/home/evie/org/declutter.org"
+     "/home/evie/org/editing.org" "/home/evie/org/finance.org"
+     "/home/evie/org/fitness.org" "/home/evie/org/four_month_plan.org"
+     "/home/evie/org/france-guide.org" "/home/evie/org/france26.org"
+     "/home/evie/org/ghc.org" "/home/evie/org/homeserver.org"
+     "/home/evie/org/jobs.org" "/home/evie/org/kitchen.org"
+     "/home/evie/org/life.org" "/home/evie/org/medical.org"
+     "/home/evie/org/nix-migration.org" "/home/evie/org/projects.org"
+     "/home/evie/org/reading.org" "/home/evie/org/staffportal.org"
+     "/home/evie/org/summer.org" "/home/evie/org/unisys.org"
+     "/home/evie/org/weeks.org" "/home/evie/org/winston.org"))
  '(safe-local-variable-directories
    '("/home/evie/staff-portal/" "/home/evie/.config/doom/"
      "/home/evie/.config/emacs/")))
